8157cf5721328958c77e62cede681da405dccbf9
emincelikkan <emincelikkan@gmail.com>
2021-08-10 15:27:42 +0300

    IBoutlets are done

--- RandomProject/LoginViewController.swift
+++ RandomProject/LoginViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var welcomeLBL: UILabel!
+    @IBOutlet weak var mobvenImage: UIImageView!
+    @IBOutlet weak var userNameLBL: UILabel!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var passwordLBL: UILabel!
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var loginBTN: UIButton!
+    @IBOutlet weak var signUpBTN: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        print("Login Button Clicked")
+        loginBTN.isEnabled = false
     }
-    */
+    
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        print("Sign Up Button Clicked")
+
+    }
+    
 
 }
